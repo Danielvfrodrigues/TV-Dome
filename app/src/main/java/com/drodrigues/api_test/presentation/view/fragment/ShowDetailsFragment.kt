6f72bca6d306b8adb80c19de98095e5000a57586
Daniel Rodrigues <danielvfrodrigues@gmail.com>
2023-03-28 17:46:26 -0300

    TVD-2: Binding Information to Show Details Screen
    This commit contains the following modifications:
    
    1. Information passed and bound to Fragment Layout

--- app/src/main/java/com/drodrigues/api_test/presentation/view/fragment/ShowDetailsFragment.kt
+++ app/src/main/java/com/drodrigues/api_test/presentation/view/fragment/ShowDetailsFragment.kt
@@ -1,45 +1,43 @@
 package com.drodrigues.api_test.presentation.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.GridLayoutManager
-import com.drodrigues.api_test.R
-import com.drodrigues.api_test.databinding.FragmentFirstBinding
-import com.drodrigues.api_test.databinding.FragmentSecondBinding
-import com.drodrigues.api_test.presentation.view.adapter.CardViewAdapter
+import com.drodrigues.api_test.databinding.FragmentShowDetailsBinding
 import com.drodrigues.api_test.presentation.viewmodel.MainViewModel
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SecondFragment : Fragment() {
+class ShowDetailsFragment : Fragment() {
 
-    private lateinit var binding: FragmentSecondBinding
+    private lateinit var binding: FragmentShowDetailsBinding
     private val mainViewModel: MainViewModel by viewModel()
-    private val args: SecondFragmentArgs by navArgs()
+    private val args: ShowDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        binding = FragmentSecondBinding.inflate(layoutInflater)
+        binding = FragmentShowDetailsBinding.inflate(layoutInflater)
 
         mainViewModel.getShowByIdLiveData.observe(viewLifecycleOwner) { show ->
             Picasso.get().load(show.imageEntity.medium).into(binding.showCover)
+            binding.showName.text = show.name
+            binding.showGenres.text = show.genres.toString()
+            binding.showSummary.text = HtmlCompat.fromHtml(show.summary, HtmlCompat.FROM_HTML_MODE_LEGACY)
         }
 
         lifecycleScope.launch {
             mainViewModel.getShowById(args.showId)
         }
 
-
         return binding.root
     }
-
 }
